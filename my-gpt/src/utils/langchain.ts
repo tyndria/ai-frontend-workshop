186fe261d3682d89f29f08993c8da6473e44e9a9
Antanina Vertsinskaya <antanina.vertsinskaya@seldon.io>
2024-04-04 12:26:12 +0100

    update generateAnswer function with more advanced functionality

--- my-gpt/src/utils/langchain.ts
+++ my-gpt/src/utils/langchain.ts
@@ -1,25 +1,54 @@
-import { OpenAI } from "@langchain/openai";
-import { PromptTemplate } from "@langchain/core/prompts";
+import { ChatOpenAI } from "@langchain/openai";
+import {
+  ChatPromptTemplate,
+  FewShotChatMessagePromptTemplate,
+} from "@langchain/core/prompts";
 
-const model = new OpenAI({
+const llm = new ChatOpenAI({
   openAIApiKey: import.meta.env.VITE_OPENAI_KEY,
   temperature: 1,
+  modelName: "gpt-4-0125-preview",
 });
 
-export async function generateAnswer(question: string) {
+export async function generateAnswer(
+  question: string,
+  promptTemplate: string = "Take the role of a Personal travel assistant, that answers questions in a consistent way.",
+) {
   let answer = "";
 
-  const prompt = PromptTemplate.fromTemplate(
-    `Take the role of the ML engineer researching Seldon products and answer the following question in detail: {question}`,
-  );
+  const examples = [
+    {
+      input: "What are the best restaurants in Amsterdam?",
+      output: "The highest rated restaurants in Amsterdam are (1), (2), (3)",
+    },
+    {
+      input: "What is the best time of the year to visit The Netherlands?",
+      output: "Summer",
+    },
+  ];
 
-  const formattedPrompt = await prompt.format({
-    question,
+  const examplePrompt = ChatPromptTemplate.fromTemplate(`User: {input}
+Assistant: {output}`);
+
+  const fewShotPrompt = new FewShotChatMessagePromptTemplate({
+    prefix: promptTemplate,
+    suffix: "User: {input} Assistant:",
+    examplePrompt,
+    examples,
+    inputVariables: ["input"],
+  });
+
+  const formattedPrompt = await fewShotPrompt.format({
+    input: question,
   });
 
   try {
-    answer = await model.invoke(formattedPrompt);
+    const result = await llm.invoke(formattedPrompt);
+
+    answer = result?.content as string;
   } catch (e) {
+    console.log(e);
+
     return "Something went wrong";
   }
 
